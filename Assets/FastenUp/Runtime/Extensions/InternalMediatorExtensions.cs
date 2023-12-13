f25a0c933f1de5c6a330470684ac6d45d7e87747
MerlinDS <merlinkolv@gmail.com>
2023-12-13 22:55:31 +0200

    Implemented testing concept of the InternalMediator generation

---
+++ Assets/FastenUp/Runtime/Extensions/InternalMediatorExtensions.cs
@@ -0,0 +1,19 @@
+﻿using FastenUp.Runtime.Bindings;
+using FastenUp.Runtime.Proxies;
+
+namespace FastenUp.Runtime.Extensions
+{
+    public static class InternalMediatorExtensions
+    {
+        public static void UpdateProxy<T>(this ref DataProxy<T> proxy, string name, IBindingPoint bindingPoint)
+        {
+            if(bindingPoint.Name != name)//TODO: add proper check
+                return;
+            
+            if(!bindingPoint.CanBind<T>())
+                return;
+            
+            proxy.Bind(bindingPoint.As<T>());
+        }
+    }
+}
