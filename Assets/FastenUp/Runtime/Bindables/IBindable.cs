d11933851840babb63798f3220d2777d80658cfd
MerlinDS <merlinkolv@gmail.com>
2023-12-17 14:14:06 +0200

    Added basic documentation

--- Assets/FastenUp/Runtime/Bindables/IBindable.cs
+++ Assets/FastenUp/Runtime/Bindables/IBindable.cs
@@ -1,17 +1,28 @@
+using FastenUp.Runtime.Base;
 using FastenUp.Runtime.Delegates;
 
 namespace FastenUp.Runtime.Bindables
 {
+    /// <summary>
+    /// Implementations of this interface will be bound to an <see cref="IBindPoint{T}"/>.
+    /// It provides communication between the unity components and bind points in a <see cref="IMediator"/>.
+    /// </summary>
     public interface IBindable
     {
         string Name { get; }
     }
 
+    /// <remarks>Provides one-way binding</remarks>
+    /// <typeparam name="T">Type of the value that will be set by the bindable component</typeparam>
+    /// <inheritdoc cref="IBindable"/>
     public interface IBindable<in T> : IBindable
     {
         void SetValue(T value);
     }
 
+    /// <remarks>Provides two-way binding</remarks>
+    /// <typeparam name="T">Type of the value that will be set or get by the bindable component</typeparam>
+    /// <inheritdoc cref="IBindable"/>
     public interface IGettableBindable<T> : IBindable<T>
     {
         public event OnBindableChanged OnBindableChanged;
