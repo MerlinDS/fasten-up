178fdabb8dc825a47a52be554ee438e8fd7e565e
MerlinDS <merlinkolv@gmail.com>
2023-12-18 21:31:46 +0200

    Implemented BindableButton.cs

---
+++ Assets/FastenUp.Tests/Bindables/BindableButtonTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using FastenUp.Runtime.Bindables;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityTestingAssist.Runtime;
+
+namespace FastenUp.Tests.Bindables
+{
+    [TestFixture]
+    [TestOf(typeof(BindableButton))]
+    public class BindableButtonTest
+    {
+        [Test]
+        public void Set_Value_When_value_is_action_Should_add_listener_to_click()
+        {
+            //Arrange
+            var expected = Substitute.For<UnityAction>();
+            var sut = CreateSut();
+            var button = sut.GetComponent<Button>();
+            //Act
+            sut.SetValue(expected);
+            //Assert
+            button.onClick.Invoke();
+            expected.Received(1).Invoke();
+        }
+
+        [Test]
+        public void Set_Value_When_value_is_null_Should_not_throw_exceptions()
+        {
+            //Arrange
+            var sut = CreateSut();
+            Action act = () => sut.SetValue(null);
+            //Act & Assert
+            act.Should().NotThrow();
+        }
+
+        [Test]
+        public void OnDisable_Should_remove_all_listeners()
+        {
+            //Arrange
+            var sut = CreateSut();
+            var button = sut.GetComponent<Button>();
+            var action = Substitute.For<UnityAction>();
+            sut.SetValue(action);
+            //Act
+            sut.ExecuteOnDisable();
+            //Assert
+            button.onClick.Invoke();
+            action.DidNotReceive().Invoke();
+        }
+
+        private static BindableButton CreateSut()
+        {
+            var gameObject = new GameObject(nameof(BindableButtonTest));
+            var sut = gameObject.AddComponent<BindableButton>();
+            sut.ExecuteAwake();
+            return sut;
+        }
+    }
+}
---
+++ Assets/FastenUp/Runtime/Bindables/BindableButton.cs
@@ -0,0 +1,38 @@
+﻿using FastenUp.Runtime.Utils;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace FastenUp.Runtime.Bindables
+{
+    /// <summary>
+    /// The one-way <see cref="IBindable"/> binds a value to the component with <see cref="UnityAction"/> field.
+    /// </summary>
+    [RequireComponent(typeof(Button))]
+    [AddComponentMenu(FastenUpComponentMenu.BaseMenu + nameof(BindableButton) , 2)]
+    public class BindableButton : BaseBindable, IBindable<UnityAction>
+    {
+        private Button _component;
+
+        private void Awake()
+        {
+            TryGetComponent(out _component);
+        }
+
+        /// <inheritdoc />
+        public void SetValue(UnityAction value)
+        {   
+            if(value is null)
+                return;
+            
+            _component.onClick.AddListener(value);
+        }
+
+        /// <inheritdoc />
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            _component.onClick.RemoveAllListeners();
+        }
+    }
+}
