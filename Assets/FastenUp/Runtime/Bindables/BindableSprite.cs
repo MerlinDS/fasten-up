a20aeaf99c7a397d3887ae1fe3c9beabeaa70fb9
MerlinDS <merlinkolv@gmail.com>
2023-12-18 23:59:07 +0200

    Implemented BindableSprite.cs for bind components with sprites

---
+++ Assets/FastenUp/Runtime/Bindables/BindableSprite.cs
@@ -0,0 +1,24 @@
+﻿using FastenUp.Runtime.Adapters;
+using UnityEngine;
+
+namespace FastenUp.Runtime.Bindables
+{
+    public sealed partial class BindableSprite : BaseBindable, IBindable<Sprite>
+    {
+        private IComponentFieldAdapter<Sprite> _adapter;
+
+        private void Awake()
+        {
+            if (ImageAdapter.TryCreate(gameObject, out _adapter))
+                return;
+
+            SpriteRendererAdapter.TryCreate(gameObject, out _adapter);
+        }
+
+        public void SetValue(Sprite value)
+        {
+            if (_adapter is not null) 
+                _adapter.Value = value;
+        }
+    }
+}
