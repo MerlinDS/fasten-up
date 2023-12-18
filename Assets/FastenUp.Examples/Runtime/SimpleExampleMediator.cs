b6a7f580669a466169f69deeac3a0314e513ebab
MerlinDS <merlinkolv@gmail.com>
2023-12-19 00:00:03 +0200

    Examples updated with new bindlables

--- Assets/FastenUp.Examples/Runtime/SimpleExampleMediator.cs
+++ Assets/FastenUp.Examples/Runtime/SimpleExampleMediator.cs
@@ -9,15 +9,41 @@ namespace FastenUp.Examples.Runtime
         private BindPoint<string> Text { get; } = new();
 
         private BindPoint<Color> Color { get; } = new();
-        
+
+        public BindPoint<bool> Toggle { get; } = new();
+
+        public BindPoint<string> Image { get; } = new();
+
         private BindPoint<UnityAction> ButtonClick { get; } = new();
         private BindPoint<int> ColorChangeCount { get; } = new();
 
+
         private void Awake()
         {
-            Text.Value = "Hello World!";
+            Color.OnValueChanged += OnColorValueChanged;
+            Toggle.OnValueChanged += OnToggleValueChanged;
+
+            Toggle.Value = true;
             Color.Value = UnityEngine.Color.yellow;
             ButtonClick.Value = ChangeColor;
+            Image.Value = "FastenUp - Icon";
+        }
+
+        private void OnDestroy()
+        {
+            Color.OnValueChanged -= OnColorValueChanged;
+            Toggle.OnValueChanged -= OnToggleValueChanged;
+        }
+
+        private void OnColorValueChanged(Color color)
+        {
+            ColorChangeCount.Value++;
+        }
+
+        private void OnToggleValueChanged(bool value)
+        {
+            Text.Value = value ? "Hello World!" : "Toggle is off!";
+            Image.Value = value ? "FastenUp - Icon" : null;
         }
 
         private void ChangeColor()
@@ -25,7 +51,6 @@ namespace FastenUp.Examples.Runtime
             Color.Value = Color.Value == UnityEngine.Color.yellow
                 ? UnityEngine.Color.red
                 : UnityEngine.Color.yellow;
-            ColorChangeCount.Value++;
         }
     }
 }
