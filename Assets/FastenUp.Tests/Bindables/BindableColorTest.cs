3f8ac6e4877414d10c3abd01c3ea9ef9c1fc7a0c
MerlinDS <merlinkolv@gmail.com>
2023-12-18 20:27:11 +0200

    Implemented BindableColor.cs

---
+++ Assets/FastenUp.Tests/Bindables/BindableColorTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using FastenUp.Runtime.Bindables;
+using FluentAssertions;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityTestingAssist.Runtime;
+
+namespace FastenUp.Tests.Bindables
+{
+    [TestFixture]
+    [TestOf(typeof(BindableColor))]
+    public class BindableColorTest
+    {
+        [Test]
+        public void SetValue_When_gameObject_has_Image_Should_set_color_to_Image()
+        {
+            //Arrange
+            var expected = Color.red;
+            var sut = CreateSut(o=>o.AddComponent<Image>());
+            //Act
+            sut.SetValue(expected);
+            //Assert
+            sut.GetComponent<Image>().color.Should().Be(expected);
+        }
+
+        [Test]
+        public void SetValue_When_gameObject_has_SpriteRenderer_Should_set_color_to_SpriteRenderer()
+        {
+            //Arrange
+            var expected = Color.red;
+            var sut = CreateSut(o=>o.AddComponent<SpriteRenderer>());
+            //Act
+            sut.SetValue(expected);
+            //Assert
+            sut.GetComponent<SpriteRenderer>().color.Should().Be(expected);
+        }
+        
+        [Test]
+        public void SetValue_When_gameObject_has_no_suitable_component_Should_not_throw()
+        {
+            //Arrange
+            var sut = CreateSut();
+            //Act
+            Action action = () => sut.SetValue(Color.red);
+            //Assert
+            action.Should().NotThrow();
+        }
+
+        private static BindableColor CreateSut(Action<GameObject> factory = null)
+        {
+            var gameObject = new GameObject(nameof(BindableColorTest));
+            factory?.Invoke(gameObject);
+            var sut = gameObject.AddComponent<BindableColor>();
+            sut.ExecuteAwake();
+            return sut;
+        }
+    }
+}
---
+++ Assets/FastenUp/Runtime/Bindables/BindableColor.cs
@@ -0,0 +1,60 @@
+﻿using FastenUp.Runtime.Utils;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FastenUp.Runtime.Bindables
+{
+    /// <summary>
+    /// The one-way <see cref="IBindable"/> binds a value to the component with <see cref="Color"/> field.
+    /// </summary>
+    [AddComponentMenu(FastenUpComponentMenu.BaseMenu + nameof(BindableColor) , 1)]
+    public class BindableColor : BaseBindable, IBindable<Color>
+    {
+        private IColorFieldAdapter _adapter;
+
+        private void Awake()
+        {
+            if (TryGetComponent<Graphic>(out var graphic))
+            {
+                _adapter = new GraphicColorAdapter(graphic);
+                return;
+            }
+
+            if (TryGetComponent<SpriteRenderer>(out var spriteRenderer))
+                _adapter = new SpriteRendererColorAdapter(spriteRenderer);
+        }
+
+        /// <inheritdoc />
+        public void SetValue(Color value) =>
+            _adapter?.SetColor(value);
+
+        private sealed class GraphicColorAdapter : IColorFieldAdapter
+        {
+            private readonly Graphic _graphic;
+
+            public GraphicColorAdapter(Graphic graphic) =>
+                _graphic = graphic;
+
+            /// <inheritdoc />
+            public void SetColor(Color value) =>
+                _graphic.color = value;
+        }
+
+        private sealed class SpriteRendererColorAdapter : IColorFieldAdapter
+        {
+            private readonly SpriteRenderer _spriteRenderer;
+
+            public SpriteRendererColorAdapter(SpriteRenderer spriteRenderer) =>
+                _spriteRenderer = spriteRenderer;
+
+            /// <inheritdoc />
+            public void SetColor(Color value) =>
+                _spriteRenderer.color = value;
+        }
+
+        private interface IColorFieldAdapter
+        {
+            void SetColor(Color value);
+        }
+    }
+}
