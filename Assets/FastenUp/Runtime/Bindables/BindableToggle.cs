efe113369e77b3154d728ad6f4272e2a054a171c
MerlinDS <merlinkolv@gmail.com>
2023-12-18 23:59:47 +0200

    Implemented BindableToggle.cs for bind Toggle component

---
+++ Assets/FastenUp/Runtime/Bindables/BindableToggle.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace FastenUp.Runtime.Bindables
+{
+    [RequireComponent(typeof(Toggle))]
+    public sealed class BindableToggle : BaseBindable, IGettableBindable<bool>
+    {
+        private Toggle _component;
+
+        private void Awake()
+        {
+            if (TryGetComponent(out _component))
+                _component.onValueChanged.AddListener(OnValueChanged);
+        }
+
+        /// <inheritdoc />
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            if (_component != null)
+                _component.onValueChanged.RemoveListener(OnValueChanged);
+        }
+
+        /// <inheritdoc />
+        public void SetValue(bool value)
+        {
+            _component.SetIsOnWithoutNotify(value);
+        }
+
+        /// <inheritdoc />
+        public bool GetValue()
+        {
+            return _component.isOn;
+        }
+
+        private void OnValueChanged(bool value) => 
+            InvokeOnBindableChanged();
+    }
+}
