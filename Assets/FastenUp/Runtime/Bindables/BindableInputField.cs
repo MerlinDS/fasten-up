9408fe05d8181751ed74c4e93b6eed4a14ad413d
MerlinDS <merlinkolv@gmail.com>
2023-12-19 10:36:19 +0200

    Implemented BindableInputField for the TMP_InputField binding

---
+++ Assets/FastenUp/Runtime/Bindables/BindableInputField.cs
@@ -0,0 +1,43 @@
+﻿using FastenUp.Runtime.Utils;
+using TMPro;
+using UnityEngine;
+
+namespace FastenUp.Runtime.Bindables
+{
+    /// <summary>
+    /// The two-way <see cref="IBindable"/> binds a value to the component with <see cref="TMP_InputField"/> field.
+    /// </summary> 
+    [RequireComponent(typeof(TMP_InputField))]
+    [AddComponentMenu(FastenUpComponentMenu.BaseMenu + nameof(BindableInputField) , 8)]
+    public class BindableInputField : BaseBindable, IGettableBindable<string>
+    {
+        private TMP_InputField _inputField;
+        
+        private void Awake()
+        {
+            _inputField = GetComponent<TMP_InputField>();
+            _inputField.onValueChanged.AddListener(OnValueChanged);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            _inputField.onValueChanged.RemoveListener(OnValueChanged);
+        }
+        
+        /// <inheritdoc />
+        public void SetValue(string value)
+        {
+            _inputField.SetTextWithoutNotify(value);
+        }
+
+        /// <inheritdoc />
+        public string GetValue()
+        {
+            return _inputField.text;
+        }
+        
+        private void OnValueChanged(string _) => 
+            InvokeOnBindableChanged();
+    }
+}
