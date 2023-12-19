67a6f2249538e38573b705c52161581a7925ef8d
MerlinDS <merlinkolv@gmail.com>
2023-12-19 09:12:25 +0200

    Refactored Bindables

--- Assets/FastenUp/Runtime/Bindables/BindableToggle.cs
+++ Assets/FastenUp/Runtime/Bindables/BindableToggle.cs
@@ -6,35 +6,34 @@ namespace FastenUp.Runtime.Bindables
     [RequireComponent(typeof(Toggle))]
     public sealed class BindableToggle : BaseBindable, IGettableBindable<bool>
     {
-        private Toggle _component;
+        private Toggle _toggle;
 
         private void Awake()
         {
-            if (TryGetComponent(out _component))
-                _component.onValueChanged.AddListener(OnValueChanged);
+            _toggle = GetComponent<Toggle>();
+            _toggle.onValueChanged.AddListener(OnValueChanged);
         }
 
         /// <inheritdoc />
         protected override void OnDisable()
         {
             base.OnDisable();
-            if (_component != null)
-                _component.onValueChanged.RemoveListener(OnValueChanged);
+            _toggle.onValueChanged.RemoveListener(OnValueChanged);
         }
 
         /// <inheritdoc />
         public void SetValue(bool value)
         {
-            _component.SetIsOnWithoutNotify(value);
+            _toggle.SetIsOnWithoutNotify(value);
         }
 
         /// <inheritdoc />
         public bool GetValue()
         {
-            return _component.isOn;
+            return _toggle.isOn;
         }
 
-        private void OnValueChanged(bool value) => 
+        private void OnValueChanged(bool value) =>
             InvokeOnBindableChanged();
     }
 }
