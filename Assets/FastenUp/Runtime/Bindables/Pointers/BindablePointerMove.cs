b9bf3d0347dd52a724f0523f014bf89a6d659d96
MerlinDS <merlinkolv@gmail.com>
2023-12-20 16:19:32 +0200

    Implemented BindablePointerMove for pointer move event handling

---
+++ Assets/FastenUp/Runtime/Bindables/Pointers/BindablePointerMove.cs
@@ -0,0 +1,19 @@
+﻿using FastenUp.Runtime.Utils;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace FastenUp.Runtime.Bindables.Pointers
+{
+    /// <summary>
+    /// This class is used to bind to the <see cref="IPointerUpHandler"/> interface.
+    /// </summary>
+    [AddComponentMenu(FastenUpComponentMenu.PointerMenu + nameof(BindablePointerMove), 0)]
+    public sealed class BindablePointerMove : BindablePointer, IPointerMoveHandler
+    {
+        /// <inheritdoc />
+        public void OnPointerMove(PointerEventData eventData)
+        {
+            OnPointerEvent(eventData);
+        }
+    }
+}
