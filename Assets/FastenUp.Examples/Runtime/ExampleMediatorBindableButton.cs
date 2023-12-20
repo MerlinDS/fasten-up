85fa8aeaf7460896a11116ac26074299b52ea221
MerlinDS <merlinkolv@gmail.com>
2023-12-20 16:24:45 +0200

    Updated examples with the new BindActions system

--- Assets/FastenUp.Examples/Runtime/ExampleMediatorBindableButton.cs
+++ Assets/FastenUp.Examples/Runtime/ExampleMediatorBindableButton.cs
@@ -1,22 +1,26 @@
 ﻿using FastenUp.Runtime.Base;
 using UnityEngine;
-using UnityEngine.Events;
 
 namespace FastenUp.Examples.Runtime
 {
     internal partial class ExampleMediatorBindableButton : MonoBehaviour, IMediator
     {
-        private BindPoint<UnityAction> OnClick { get; } = new();
+        private BindAction OnClick { get; } = new();
         private BindPoint<int> ClickCount { get; } = new();
 
         private void Awake()
         {
-            OnClick.Value = () => ClickCount.Value++;
+            OnClick.AddListener(OnClickHandler);
+        }
+
+        private void OnClickHandler()
+        {
+            ClickCount.Value++;
         }
 
         private void OnDestroy()
         {
-            OnClick.Value = null;
+            OnClick.Dispose();
         }
     }
 }
