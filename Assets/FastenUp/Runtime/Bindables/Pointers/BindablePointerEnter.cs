60d571829f1e71a3d1bb28779cfb7957d6b4c887
MerlinDS <merlinkolv@gmail.com>
2023-12-20 16:17:00 +0200

    Implemented BindablePointerEnter for pointer enter event handling

---
+++ Assets/FastenUp/Runtime/Bindables/Pointers/BindablePointerEnter.cs
@@ -0,0 +1,19 @@
+﻿using FastenUp.Runtime.Utils;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace FastenUp.Runtime.Bindables.Pointers
+{
+    /// <summary>
+    /// This class is used to bind to the <see cref="IPointerEnterHandler"/> interface.
+    /// </summary> 
+    [AddComponentMenu(FastenUpComponentMenu.PointerMenu + nameof(BindablePointerEnter), 0)]
+    public sealed class BindablePointerEnter : BindablePointer, IPointerEnterHandler
+    {
+        /// <inheritdoc />
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            OnPointerEvent(eventData);
+        }
+    }
+}
