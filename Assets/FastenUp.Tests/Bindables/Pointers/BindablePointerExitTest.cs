be6ead070c679321c0f6e6c554ddad8cbb1d4e5d
MerlinDS <merlinkolv@gmail.com>
2023-12-20 16:18:20 +0200

    Implemented BindablePointerExit for pointer exit event handling

---
+++ Assets/FastenUp.Tests/Bindables/Pointers/BindablePointerExitTest.cs
@@ -0,0 +1,30 @@
+﻿using FastenUp.Runtime.Bindables.Pointers;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace FastenUp.Tests.Bindables.Pointers
+{
+    [TestFixture]
+    [TestOf(typeof(BindablePointerExit))]
+    public class BindablePointerExitTest
+    {
+        [Test]
+        public void OnPointerExit_When_has_listener_Should_invoke_event()
+        {
+            // Arrange
+            var actual = Substitute.For<UnityAction<PointerEventData>>();
+            var sut = CreateSut();
+            sut.AddListener(actual);
+            // Act
+            sut.OnPointerExit(new PointerEventData(EventSystem.current));
+            // Assert
+            actual.Received(1).Invoke(Arg.Any<PointerEventData>());
+        }
+        
+        private static BindablePointerExit CreateSut() => 
+            new GameObject().AddComponent<BindablePointerExit>();
+    }
+}
---
+++ Assets/FastenUp/Runtime/Bindables/Pointers/BindablePointerExit.cs
@@ -0,0 +1,19 @@
+﻿using FastenUp.Runtime.Utils;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace FastenUp.Runtime.Bindables.Pointers
+{
+    /// <summary>
+    /// This class is used to bind to the <see cref="IPointerExitHandler"/> interface.
+    /// </summary> 
+    [AddComponentMenu(FastenUpComponentMenu.PointerMenu + nameof(BindablePointerExit), 0)]
+    public sealed class BindablePointerExit : BindablePointer, IPointerExitHandler
+    {
+        /// <inheritdoc />
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            OnPointerEvent(eventData);
+        }
+    }
+}
