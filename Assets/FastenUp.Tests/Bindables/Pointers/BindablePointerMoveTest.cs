241ffaa7128d2dbcfece6c5a167791f5376edf2f
MerlinDS <merlinkolv@gmail.com>
2023-12-20 16:19:32 +0200

    Implemented BindablePointerMove for pointer move event handling

---
+++ Assets/FastenUp.Tests/Bindables/Pointers/BindablePointerMoveTest.cs
@@ -0,0 +1,30 @@
+﻿using FastenUp.Runtime.Bindables.Pointers;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace FastenUp.Tests.Bindables.Pointers
+{
+    [TestFixture]
+    [TestOf(typeof(BindablePointerMove))]
+    public class BindablePointerMoveTest
+    {
+        [Test]
+        public void OnPointerMove_When_has_listener_Should_invoke_event()
+        {
+            // Arrange
+            var actual = Substitute.For<UnityAction<PointerEventData>>();
+            var sut = CreateSut();
+            sut.AddListener(actual);
+            // Act
+            sut.OnPointerMove(new PointerEventData(EventSystem.current));
+            // Assert
+            actual.Received(1).Invoke(Arg.Any<PointerEventData>());
+        }
+        
+        private static BindablePointerMove CreateSut() => 
+            new GameObject().AddComponent<BindablePointerMove>();
+    }
+}
