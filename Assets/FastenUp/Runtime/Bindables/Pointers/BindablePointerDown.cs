3cb8d2d9ba4e425657b780ee4e0d9dd3bb300043
MerlinDS <merlinkolv@gmail.com>
2023-12-20 16:15:14 +0200

    Implemented BindablePointerDown for pointer down event handling

---
+++ Assets/FastenUp/Runtime/Bindables/Pointers/BindablePointerDown.cs
@@ -0,0 +1,19 @@
+﻿using FastenUp.Runtime.Utils;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace FastenUp.Runtime.Bindables.Pointers
+{
+    /// <summary>
+    /// This class is used to bind to the <see cref="IPointerDownHandler"/> interface.
+    /// </summary>
+    [AddComponentMenu(FastenUpComponentMenu.PointerMenu + nameof(BindablePointerDown), 0)]
+    public sealed class BindablePointerDown : BindablePointer, IPointerDownHandler
+    {
+        /// <inheritdoc />
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            OnPointerEvent(eventData);
+        }
+    }
+}
