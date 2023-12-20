d74076cc023ef9c8a908b6e2173cc421fef3c9b4
MerlinDS <merlinkolv@gmail.com>
2023-12-20 15:25:22 +0200

    Added unit tests for new methods in BindUtilities

--- Assets/FastenUp.Tests/Utils/BindUtilitiesTest.cs
+++ Assets/FastenUp.Tests/Utils/BindUtilitiesTest.cs
@@ -110,5 +110,106 @@ namespace FastenUp.Tests.Utils
             //Assert
             point.DidNotReceive().Remove(Arg.Any<IBindable<bool>>());
         }
+        
+        [Test]
+        public void TryBind_When_action_and_bindable_are_suited_Should_Add_bindable_to_action()
+        {
+            //Arrange
+            var bindable = Substitute.For<IBindable, IBindableListener<bool>>();
+            bindable.Name.Returns("Test");
+            var action = Substitute.For<IInternalBindAction<bool>>();
+            //Act
+            BindUtilities.TryBind(action,"Test", bindable);
+            //Assert
+            action.Received(1).AddListener(bindable.As<IBindableListener<bool>>());
+        }
+        
+        [Test]
+        public void TryBind_When_action_and_bindable_have_different_names_Should_not_Add_bindable_to_action()
+        {
+            //Arrange
+            var bindable = Substitute.For<IBindable, IBindableListener<bool>>();
+            bindable.Name.Returns("Test");
+            var action = Substitute.For<IInternalBindAction<bool>>();
+            //Act
+            BindUtilities.TryBind(action,"Test2", bindable);
+            //Assert
+            action.DidNotReceive().AddListener(bindable.As<IBindableListener<bool>>());
+        }
+        
+        [Test]
+        public void TryBind_When_action_and_bindable_have_different_types_Should_not_Add_bindable_to_action()
+        {
+            //Arrange
+            var bindable = Substitute.For<IBindable, IBindableListener<bool>>();
+            bindable.Name.Returns("Test");
+            var action = Substitute.For<IInternalBindAction<int>>();
+            //Act
+            BindUtilities.TryBind(action,"Test", bindable);
+            //Assert
+            action.DidNotReceive().AddListener(Arg.Any<IBindableListener<int>>());
+        }
+        
+        [Test]
+        public void TryBind_When_bindable_is_null_Should_not_Add_bindable_to_action()
+        {
+            //Arrange
+            var action = Substitute.For<IInternalBindAction<bool>>();
+            //Act
+            BindUtilities.TryBind(action,"Test", null);
+            //Assert
+            action.DidNotReceive().AddListener(Arg.Any<IBindableListener<bool>>());
+        }
+        
+        [Test]
+        public void TryUnbind_When_action_and_bindable_are_suited_Should_Remove_bindable_from_action()
+        {
+            //Arrange
+            var bindable = Substitute.For<IBindable, IBindableListener<bool>>();
+            bindable.Name.Returns("Test");
+            var action = Substitute.For<IInternalBindAction<bool>>();
+            //Act
+            BindUtilities.TryUnbind(action,"Test", bindable);
+            //Assert
+            action.Received(1).RemoveListener(bindable.As<IBindableListener<bool>>());
+        }
+        
+        [Test]
+        public void TryUnbind_When_action_and_bindable_have_different_names_Should_not_Remove_bindable_from_action()
+        {
+            //Arrange
+            var bindable = Substitute.For<IBindable, IBindableListener<bool>>();
+            bindable.Name.Returns("Test");
+            var action = Substitute.For<IInternalBindAction<bool>>();
+            //Act
+            BindUtilities.TryBind(action,"Test2", bindable);
+            //Assert
+            action.DidNotReceive().RemoveListener(bindable.As<IBindableListener<bool>>());
+        }
+        
+        [Test]
+        public void TryUnbind_When_action_and_bindable_have_different_types_Should_not_Remove_bindable_from_action()
+        {
+            //Arrange
+            var bindable = Substitute.For<IBindable, IBindableListener<bool>>();
+            bindable.Name.Returns("Test");
+            var action = Substitute.For<IInternalBindAction<int>>();
+            //Act
+            BindUtilities.TryBind(action,"Test", bindable);
+            //Assert
+            action.DidNotReceive().RemoveListener(Arg.Any<IBindableListener<int>>());
+        }
+        
+        [Test]
+        public void TryUnbind_When_bindable_is_null_Should_not_Remove_bindable_from_action()
+        {
+            //Arrange
+            var action = Substitute.For<IInternalBindAction<bool>>();
+            //Act
+            BindUtilities.TryBind(action,"Test", null);
+            //Assert
+            action.DidNotReceive().RemoveListener(Arg.Any<IBindableListener<bool>>());
+        }
+        
     }
 }
