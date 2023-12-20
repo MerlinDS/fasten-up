b7aebb0a6bf159582bf8b2fd8d52d52a43232a64
MerlinDS <merlinkolv@gmail.com>
2023-12-20 16:12:48 +0200

    Implemented BindablePointerClick for pointer click handling

---
+++ Assets/FastenUp/Runtime/Bindables/Pointers/BindablePointerClick.cs
@@ -0,0 +1,19 @@
+﻿using FastenUp.Runtime.Utils;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace FastenUp.Runtime.Bindables.Pointers
+{
+    /// <summary>
+    /// This class is used to bind to the <see cref="IPointerClickHandler"/> interface.
+    /// </summary> 
+    [AddComponentMenu(FastenUpComponentMenu.PointerMenu + nameof(BindablePointerClick), 0)]
+    public sealed class BindablePointerClick : BindablePointer, IPointerClickHandler
+    {
+        /// <inheritdoc />
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            OnPointerEvent(eventData);
+        }
+    }
+}
