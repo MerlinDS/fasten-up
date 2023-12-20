2b160a81515b90f602f9bf0c774371b06c4abee0
MerlinDS <merlinkolv@gmail.com>
2023-12-20 16:07:20 +0200

    Implemented BindablePointer

---
+++ Assets/FastenUp/Runtime/Bindables/Pointers/BindablePointer.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace FastenUp.Runtime.Bindables.Pointers
+{
+    public abstract class BindablePointer : BaseBindable,
+        IBindableListener<UnityAction>,
+        IBindableListener<UnityAction<PointerEventData>>
+    {
+        private readonly UnityEvent _event = new();
+        private readonly UnityEvent<PointerEventData> _eventWithData = new();
+
+        protected void OnPointerEvent(PointerEventData eventData)
+        {
+            _eventWithData.Invoke(eventData);
+            _event.Invoke();
+        }
+
+        /// <inheritdoc />
+        public void AddListener(UnityAction action)
+        {
+            if (action is not null)
+                _event.AddListener(action);
+        }
+
+        /// <inheritdoc />
+        public void RemoveListener(UnityAction action)
+        {
+            if (action is not null)
+                _event.RemoveListener(action);
+        }
+
+        /// <inheritdoc />
+        public void AddListener(UnityAction<PointerEventData> action)
+        {
+            if (action is not null)
+                _eventWithData.AddListener(action);
+        }
+
+        /// <inheritdoc />
+        public void RemoveListener(UnityAction<PointerEventData> action)
+        {
+            if (action is not null)
+                _eventWithData.RemoveListener(action);
+        }
+
+        /// <inheritdoc />
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            _event.RemoveAllListeners();
+            _eventWithData.RemoveAllListeners();
+        }
+    }
+}
