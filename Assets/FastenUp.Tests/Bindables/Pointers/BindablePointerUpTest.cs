c39b565437883b95d8897781460d34b009cf64b4
MerlinDS <merlinkolv@gmail.com>
2023-12-20 16:21:19 +0200

    Implemented BindablePointerUp for pointer up event handling

---
+++ Assets/FastenUp.Tests/Bindables/Pointers/BindablePointerUpTest.cs
@@ -0,0 +1,30 @@
+﻿using FastenUp.Runtime.Bindables.Pointers;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace FastenUp.Tests.Bindables.Pointers
+{
+    [TestFixture]
+    [TestOf(typeof(BindablePointerUp))]
+    public class BindablePointerUpTest
+    {
+        [Test]
+        public void OnPointerMove_When_has_listener_Should_invoke_event()
+        {
+            // Arrange
+            var actual = Substitute.For<UnityAction<PointerEventData>>();
+            var sut = CreateSut();
+            sut.AddListener(actual);
+            // Act
+            sut.OnPointerUp(new PointerEventData(EventSystem.current));
+            // Assert
+            actual.Received(1).Invoke(Arg.Any<PointerEventData>());
+        }
+        
+        private static BindablePointerUp CreateSut() => 
+            new GameObject().AddComponent<BindablePointerUp>();
+    }
+}
---
+++ Assets/FastenUp/Runtime/Bindables/Pointers/BindablePointerUp.cs
@@ -0,0 +1,19 @@
+﻿using FastenUp.Runtime.Utils;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace FastenUp.Runtime.Bindables.Pointers
+{
+    /// <summary>
+    /// This class is used to bind to the <see cref="IPointerUpHandler"/> interface.
+    /// </summary>
+    [AddComponentMenu(FastenUpComponentMenu.PointerMenu + nameof(BindablePointerUp), 0)]
+    public sealed class BindablePointerUp : BindablePointer, IPointerUpHandler
+    {
+        /// <inheritdoc />
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            OnPointerEvent(eventData);
+        }
+    }
+}
