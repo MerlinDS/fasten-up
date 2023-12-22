89eae1d004c1f36bdf8595b574c8da61eb177edc
MerlinDS <merlinkolv@gmail.com>
2023-12-22 12:37:12 +0200

    Renamed BindablePointerDown to PointerDownBinder and move it to the Binders namespace

--- Assets/FastenUp.Tests/Binders/Pointers/PointerDownBinderTest.cs
+++ Assets/FastenUp.Tests/Binders/Pointers/PointerDownBinderTest.cs
@@ -1,15 +1,15 @@
-﻿using FastenUp.Runtime.Bindables.Pointers;
+﻿using FastenUp.Runtime.Binders.Pointers;
 using NSubstitute;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-namespace FastenUp.Tests.Bindables.Pointers
+namespace FastenUp.Tests.Binders.Pointers
 {
     [TestFixture]
-    [TestOf(typeof(BindablePointerDown))]
-    public class BindablePointerDownTest
+    [TestOf(typeof(PointerDownBinder))]
+    public class PointerDownBinderTest
     {
         [Test]
         public void OnPointerDown_When_has_listener_Should_invoke_event()
@@ -24,7 +24,7 @@ namespace FastenUp.Tests.Bindables.Pointers
             actual.Received(1).Invoke(Arg.Any<PointerEventData>());
         }
         
-        private static BindablePointerDown CreateSut() => 
-            new GameObject().AddComponent<BindablePointerDown>();
+        private static PointerDownBinder CreateSut() => 
+            new GameObject().AddComponent<PointerDownBinder>();
     }
 }
