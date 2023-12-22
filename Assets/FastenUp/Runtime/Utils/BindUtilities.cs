9b0c93f03e17340ba3f9c8a8db327573dea126a7
MerlinDS <merlinkolv@gmail.com>
2023-12-22 14:25:57 +0200

    BindUtilities: naming refactoring and  AggressiveInlining added

--- Assets/FastenUp/Runtime/Utils/BindUtilities.cs
+++ Assets/FastenUp/Runtime/Utils/BindUtilities.cs
@@ -1,32 +1,37 @@
-﻿using FastenUp.Runtime.Bindables;
+﻿using System.Runtime.CompilerServices;
+using FastenUp.Runtime.Bindables;
 using FastenUp.Runtime.Binders;
 
 namespace FastenUp.Runtime.Utils
 {
     public static class BindUtilities
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void TryBind<T>(IInternalBindable<T> bindable, IBinder binder)
         {
-            if (binder is IBinder<T> bindableT)
-                bindable.Bind(bindableT);
+            if (binder is IBinder<T> binderT)
+                bindable.Bind(binderT);
         }
         
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void TryUnbind<T>(IInternalBindable<T> bindable, IBinder binder)
         {
-            if (binder is IBinder<T> bindableT)
-                bindable.Unbind(bindableT);
+            if (binder is IBinder<T> binderT)
+                bindable.Unbind(binderT);
         }
         
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void TryBind<T>(IInternalBindableEvent<T> bindableEvent, IBinder eventBinder)
         {
-            if (eventBinder is IEventBinder<T> bindableT)
-                bindableEvent.Bind(bindableT);
+            if (eventBinder is IEventBinder<T> eventBinderT)
+                bindableEvent.Bind(eventBinderT);
         }
         
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void TryUnbind<T>(IInternalBindableEvent<T> bindableEvent, IBinder eventBinder)
         {
-            if (eventBinder is IEventBinder<T> bindableT)
-                bindableEvent.Unbind(bindableT);
+            if (eventBinder is IEventBinder<T> eventBinderT)
+                bindableEvent.Unbind(eventBinderT);
         }
     }
 }
