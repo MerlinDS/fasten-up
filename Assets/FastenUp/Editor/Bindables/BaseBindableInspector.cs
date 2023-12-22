fd97c31e2dcd95d897c9efa00cfb348c1c0d9f7f
MerlinDS <merlinkolv@gmail.com>
2023-12-22 11:31:43 +0200

    Implemented BaseBindableInspector to validate Name field in inspector

---
+++ Assets/FastenUp/Editor/Bindables/BaseBindableInspector.cs
@@ -0,0 +1,42 @@
+﻿using FastenUp.Runtime.Bindables;
+using UnityEditor;
+using UnityEngine;
+
+namespace FastenUp.Editor.Bindables
+{
+    [CustomEditor(typeof(BaseBindable), true)]
+    public class BaseBindableInspector : UnityEditor.Editor
+    {
+        private SerializedProperty _nameProperty;
+
+        private void OnEnable()
+        {
+            _nameProperty = serializedObject.FindProperty("<Name>k__BackingField");
+        }
+
+        private void OnDisable()
+        {
+            _nameProperty.Dispose();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            OnNameProperty();
+            DrawPropertiesExcluding(serializedObject, "m_Script", _nameProperty.name);
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void OnNameProperty()
+        {
+            var content = new GUIContent(_nameProperty.displayName,
+                "Name used for binding. It must be the same as the name of the property in the Mediator.");
+            
+            if (EditorGUILayout.PropertyField(_nameProperty, content))
+                _nameProperty.stringValue = _nameProperty.stringValue.Trim();
+
+            if (string.IsNullOrEmpty(_nameProperty.stringValue))
+                EditorGUILayout.HelpBox("This binder will be ignored! Name must be set!", MessageType.Warning);
+        }
+    }
+}
