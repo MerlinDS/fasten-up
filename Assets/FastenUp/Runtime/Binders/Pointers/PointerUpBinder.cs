9aca44c4ba5fb7cb1ce611d45891ae1c693db9b9
MerlinDS <merlinkolv@gmail.com>
2023-12-23 12:27:37 +0200

    Added requirement of the PointerDownBinder component to the PointerUpBinder. IPointerDownHandler must be implemented in receiver to IPointerUpHandler works properly

--- Assets/FastenUp/Runtime/Binders/Pointers/PointerUpBinder.cs
+++ Assets/FastenUp/Runtime/Binders/Pointers/PointerUpBinder.cs
@@ -7,8 +7,9 @@ namespace FastenUp.Runtime.Binders.Pointers
     /// <summary>
     /// This class is used to bind to the <see cref="IPointerUpHandler"/> interface.
     /// </summary>
+    [RequireComponent(typeof(PointerDownBinder))]
     [AddComponentMenu(FastenUpComponentMenu.EventHandlersMenu + "Pointer Up Binder", 5)]
-    [HelpURL("https://github.com/MerlinDS/fasten-up/wiki/Binders#pointer-up")]
+    [HelpURL("https://github.com/MerlinDS/fasten-up/wiki/Binders#pointerup")]
     public sealed class PointerUpBinder : PointerBinder, IPointerUpHandler
     {
         /// <inheritdoc />
