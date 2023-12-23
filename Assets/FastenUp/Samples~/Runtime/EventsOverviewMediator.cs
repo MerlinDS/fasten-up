08eb5b7cd27cd8259cfbec2fc5c8f37375bda2e4
MerlinDS <merlinkolv@gmail.com>
2023-12-23 12:30:28 +0200

    Added DisallowMultipleComponent to the VisibilityBinder

---
+++ Assets/FastenUp/Samples~/Runtime/EventsOverviewMediator.cs
@@ -0,0 +1,91 @@
+﻿using FastenUp.Runtime.Bindables;
+using FastenUp.Runtime.Mediators;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace FastenUp.Examples.Runtime
+{
+    internal partial class EventsOverviewMediator : MonoBehaviour, IMediator
+    {
+        private BindableEvent Button { get; } = new();
+
+        private Bindable<int> PressedCount { get; } = new();
+        
+        private BindableEvent<PointerEventData> PointerClick { get; } = new();
+        private Bindable<string> PositionClick { get; } = new();
+        
+        private BindableEvent<PointerEventData> PointerEnter { get; } = new();
+        private Bindable<string> PositionEnter { get; } = new();
+        
+        private BindableEvent<PointerEventData> PointerExit { get; } = new();
+        private Bindable<string> PositionExit { get; } = new();
+        
+        private BindableEvent<PointerEventData> PointerDown { get; } = new();
+        private Bindable<string> PositionDown { get; } = new();
+        
+        private BindableEvent<PointerEventData> PointerUp { get; } = new();
+        private Bindable<string> PositionUp { get; } = new();
+        
+        private BindableEvent<PointerEventData> PointerMove { get; } = new();
+        private Bindable<string> PositionMove { get; } = new();
+
+        private void Awake()
+        {
+            Button.AddListener(OnButtonClick);
+            PointerClick.AddListener(OnPointerClick);
+            PointerEnter.AddListener(OnPointerEnter);
+            PointerExit.AddListener(OnPointerExit);
+            PointerDown.AddListener(OnPointerDown);
+            PointerUp.AddListener(OnPointerUp);
+            PointerMove.AddListener(OnPointerMove);
+        }
+
+        private void OnDestroy()
+        {
+            Button.Dispose();
+            PointerClick.Dispose();
+            PointerEnter.Dispose();
+            PointerExit.Dispose();
+            PointerDown.Dispose();
+            PointerUp.Dispose();
+            PointerMove.Dispose();
+        }
+
+        private void OnButtonClick()
+        {
+            PressedCount.Value++;
+        }
+        
+        private void OnPointerClick(PointerEventData eventData)
+        {
+            PositionClick.Value = eventData.position.ToString();
+        }
+        
+        private void OnPointerEnter(PointerEventData eventData)
+        {
+            PositionEnter.Value = eventData.position.ToString();
+        }
+        
+        private void OnPointerExit(PointerEventData eventData)
+        {
+            PositionExit.Value = eventData.position.ToString();
+        }
+        
+        private void OnPointerDown(PointerEventData eventData)
+        {
+            PositionDown.Value = eventData.position.ToString();
+        }
+        
+        private void OnPointerUp(PointerEventData eventData)
+        {
+            PositionUp.Value = eventData.position.ToString();
+        }
+        
+        private void OnPointerMove(PointerEventData eventData)
+        {
+            PositionMove.Value = eventData.position.ToString();
+        }
+        
+        
+    }
+}
---
+++ Assets/FastenUp/Samples~/Runtime/GraphicsOverviewMediator.cs
@@ -0,0 +1,46 @@
+﻿using FastenUp.Runtime.Bindables;
+using FastenUp.Runtime.Mediators;
+using UnityEngine;
+
+namespace FastenUp.Examples.Runtime
+{
+    internal partial class GraphicsOverviewMediator : MonoBehaviour, IMediator
+    {
+        private Bindable<Color> Color { get; } = new() { Value = UnityEngine.Color.white };
+
+        private Bindable<Sprite> Sprite { get; } = new();
+        private Bindable<string> SpritePath { get; } = new();
+
+        private Bindable<bool> ImageVisibility { get; } = new() { Value = true };
+
+        public void ChangeToRed()
+        {
+            Color.Value = new Color(1, 0.380503F, 0.380503F);
+        }
+
+        public void ChangeToBlue()
+        {
+            Color.Value = new Color(0.380503F, 0.380503F, 1);
+        }
+
+        public void ChangeToGreen()
+        {
+            Color.Value = new Color(0.380503F, 1, 0.380503F);
+        }
+
+        public void SetSprite(bool isOn)
+        {
+            Sprite.Value = isOn ? Resources.Load<Sprite>("FastenUp - Icon") : null;
+        }
+
+        public void SetSpritePath(bool isOn)
+        {
+            SpritePath.Value = isOn ? "FastenUp - Icon" : null;
+        }
+
+        public void ChangeImageVisibility(bool isOn)
+        {
+            ImageVisibility.Value = isOn;
+        }
+    }
+}
