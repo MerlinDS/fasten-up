aa43ac6e60d0d1b1d864c1049bbb4bf19eda0a72
MerlinDS <merlinkolv@gmail.com>
2023-12-23 12:41:04 +0200

    Implemented cross notification of neighbor binders

--- Assets/FastenUp.Tests/Bindables/BindableTests.cs
+++ Assets/FastenUp.Tests/Bindables/BindableTests.cs
@@ -17,33 +17,33 @@ namespace FastenUp.Tests.Bindables
         public void Bind_When_bindable_was_not_bind_Should_set_value()
         {
             //Arrange
-            var bindable = Substitute.For<IBinder<bool>>();
+            var binder = Substitute.For<IBinder<bool>>();
             var sut = new Bindable<bool>();
             //Act & Assert
-            sut.As<IInternalBindable<bool>>().Bind(bindable);
-            bindable.Received(1).SetValue(false);
+            sut.As<IInternalBindable<bool>>().Bind(binder);
+            binder.Received(1).SetValue(false);
         }
 
         [Test]
         public void Bind_When_gettable_bindable_was_not_bind_Should_set_value_and_subscribe_to_bindable()
         {
             //Arrange
-            var bindable = Substitute.For<IGettableBinder<bool>>();
+            var binder = Substitute.For<IGettableBinder<bool>>();
             var sut = new Bindable<bool>();
             //Act & Assert
-            sut.As<IInternalBindable<bool>>().Bind(bindable);
-            bindable.Received(1).SetValue(false);
-            bindable.Received(1).OnBinderChanged += Arg.Any<OnBinderChanged>();
+            sut.As<IInternalBindable<bool>>().Bind(binder);
+            binder.Received(1).SetValue(false);
+            binder.Received(1).OnBinderChanged += Arg.Any<OnBinderChanged>();
         }
 
         [Test]
         public void Bind_When_bindable_already_was_bind_Should_throw_exception()
         {
             //Arrange
-            var bindable = Substitute.For<IBinder<bool>>();
+            var binder = Substitute.For<IBinder<bool>>();
             var sut = new Bindable<bool>();
-            sut.As<IInternalBindable<bool>>().Bind(bindable);
-            Action act = () => sut.As<IInternalBindable<bool>>().Bind(bindable);
+            sut.As<IInternalBindable<bool>>().Bind(binder);
+            Action act = () => sut.As<IInternalBindable<bool>>().Bind(binder);
             //Act & Assert
             act.Should().Throw<FastenUpException>();
         }
@@ -52,10 +52,10 @@ namespace FastenUp.Tests.Bindables
         public void Unbind_When_bindable_was_bind_Should_not_throw_exception()
         {
             //Arrange
-            var bindable = Substitute.For<IBinder<bool>>();
+            var binder = Substitute.For<IBinder<bool>>();
             var sut = new Bindable<bool>();
-            sut.As<IInternalBindable<bool>>().Bind(bindable);
-            Action act = () => sut.As<IInternalBindable<bool>>().Unbind(bindable);
+            sut.As<IInternalBindable<bool>>().Bind(binder);
+            Action act = () => sut.As<IInternalBindable<bool>>().Unbind(binder);
             //Act & Assert
             act.Should().NotThrow<Exception>();
         }
@@ -64,22 +64,22 @@ namespace FastenUp.Tests.Bindables
         public void Unbind_When_gettable_bindable_was_bind_Should_unsubscribe_from_bindable()
         {
             //Arrange
-            var bindable = Substitute.For<IGettableBinder<bool>>();
+            var binder = Substitute.For<IGettableBinder<bool>>();
             var sut = new Bindable<bool>();
-            sut.As<IInternalBindable<bool>>().Bind(bindable);
+            sut.As<IInternalBindable<bool>>().Bind(binder);
             //Act
-            sut.As<IInternalBindable<bool>>().Unbind(bindable);
+            sut.As<IInternalBindable<bool>>().Unbind(binder);
             //Assert
-            bindable.Received(1).OnBinderChanged -= Arg.Any<OnBinderChanged>();
+            binder.Received(1).OnBinderChanged -= Arg.Any<OnBinderChanged>();
         }
 
         [Test]
         public void Unbind_When_bindable_was_not_bind_Should_throw_exception()
         {
             //Arrange
-            var bindable = Substitute.For<IBinder<bool>>();
+            var binder = Substitute.For<IBinder<bool>>();
             var sut = new Bindable<bool>();
-            Action act = () => sut.As<IInternalBindable<bool>>().Unbind(bindable);
+            Action act = () => sut.As<IInternalBindable<bool>>().Unbind(binder);
             //Act & Assert
             act.Should().Throw<FastenUpException>();
         }
@@ -88,31 +88,66 @@ namespace FastenUp.Tests.Bindables
         public void Value_When_set_Should_notify_all_bindables()
         {
             //Arrange
-            var bindable1 = Substitute.For<IBinder<bool>>();
-            var bindable2 = Substitute.For<IBinder<bool>>();
+            var binder1 = Substitute.For<IBinder<bool>>();
+            var binder2 = Substitute.For<IBinder<bool>>();
             var sut = new Bindable<bool>();
-            sut.As<IInternalBindable<bool>>().Bind(bindable1);
-            sut.As<IInternalBindable<bool>>().Bind(bindable2);
+            sut.As<IInternalBindable<bool>>().Bind(binder1);
+            sut.As<IInternalBindable<bool>>().Bind(binder2);
             //Act
             sut.Value = true;
             //Assert
-            bindable1.Received(1).SetValue(true);
-            bindable2.Received(1).SetValue(true);
+            binder1.Received(1).SetValue(true);
+            binder2.Received(1).SetValue(true);
         }
 
         [Test]
         public void OnValueChanged_When_gettable_bindable_value_changed_Should_update_value_and_invoke_event()
         {
             //Arrange
-            var bindable = Substitute.For<IGettableBinder<bool>>();
-            bindable.GetValue().Returns(true);
+            var binder = Substitute.For<IGettableBinder<bool>>();
+            binder.GetValue().Returns(true);
 
             var sut = new Bindable<bool>();
-            sut.As<IInternalBindable<bool>>().Bind(bindable);
+            sut.As<IInternalBindable<bool>>().Bind(binder);
             //Act
-            bindable.OnBinderChanged += Raise.Event<OnBinderChanged>(bindable);
+            binder.OnBinderChanged += Raise.Event<OnBinderChanged>(binder);
             //Assert
             sut.Value.Should().BeTrue();
+        }
+
+        [Test]
+        public void
+            OnValueChanged_When_gettable_bindable_value_changed_but_sent_invalid_date_Should_not_update_value_and_invoke_event()
+        {
+            //Arrange
+            var binder = Substitute.For<IGettableBinder<bool>>();
+            binder.GetValue().Returns(true);
+
+            var sut = new Bindable<bool>();
+            sut.As<IInternalBindable<bool>>().Bind(binder);
+            //Act
+            binder.OnBinderChanged += Raise.Event<OnBinderChanged>
+                (Substitute.For<IBinder>());
+            //Assert
+            sut.Value.Should().BeFalse();
+        }
+
+        [Test]
+        public void OnValueChanged_When_gettable_bindable_value_changed_Should_notify_other_binders()
+        {
+            //Arrange
+            var other = Substitute.For<IBinder<bool>>();
+            var binder = Substitute.For<IGettableBinder<bool>>();
+            binder.GetValue().Returns(true);
+
+            var sut = new Bindable<bool>();
+            sut.As<IInternalBindable<bool>>().Bind(binder);
+            sut.As<IInternalBindable<bool>>().Bind(other);
+            //Act
+            binder.OnBinderChanged += Raise.Event<OnBinderChanged>(binder);
+            //Assert
+            binder.DidNotReceive().SetValue(true);
+            other.Received(1).SetValue(true);
         }
     }
 }
--- Assets/FastenUp/Runtime/Bindables/Bindable.cs
+++ Assets/FastenUp/Runtime/Bindables/Bindable.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using FastenUp.Runtime.Binders;
 using FastenUp.Runtime.Exceptions;
 
@@ -7,42 +8,47 @@ namespace FastenUp.Runtime.Bindables
 {
     public sealed class Bindable<T> : IBindable<T>, IInternalBindable<T>
     {
-        private readonly HashSet<IBinder<T>> _bindables = new(1);
+        private readonly HashSet<IBinder<T>> _binders = new(1);
 
         private T _value;
+
+        public Bindable(T value = default)
+        {
+            _value = value;
+        }
 
         public T Value
         {
             get => _value;
             set
             {
-                foreach (var bindable in _bindables)
-                    bindable.SetValue(value);
-                
-                ChangeValue(value);
+                UpdateBinders(value);
+                ChangeValueAndNotify(value);
             }
         }
 
         public event Action<T> OnValueChanged;
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         void IInternalBindable<T>.Bind(IBinder<T> binder)
         {
-            if (_bindables.Contains(binder))
+            if (_binders.Contains(binder))
                 throw new FastenUpException($"{nameof(binder)} already bind to the {nameof(Bindable<T>)}.");
 
             binder.SetValue(_value);
-            _bindables.Add(binder);
+            _binders.Add(binder);
 
             if (binder is IGettableBinder<T> bindableGettable)
                 bindableGettable.OnBinderChanged += OnValueChangedHandler;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         void IInternalBindable<T>.Unbind(IBinder<T> binder)
         {
-            if (!_bindables.Contains(binder))
+            if (!_binders.Contains(binder))
                 throw new FastenUpException($"{nameof(binder)} not bind to the {nameof(Bindable<T>)}.");
 
-            _bindables.Remove(binder);
+            _binders.Remove(binder);
             
             if (binder is IGettableBinder<T> bindableGettable)
                 bindableGettable.OnBinderChanged -= OnValueChangedHandler;
@@ -50,11 +56,28 @@ namespace FastenUp.Runtime.Bindables
 
         private void OnValueChangedHandler(IBinder binder)
         {
-            if (binder is IGettableBinder<T> bindableT)
-                ChangeValue(bindableT.GetValue());
+            if (binder is not IGettableBinder<T> bindableT)
+                return;
+
+            var value = bindableT.GetValue();
+            UpdateBinders(value, bindableT);
+            ChangeValueAndNotify(value);
         }
 
-        private void ChangeValue(T value)
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void UpdateBinders(T value, IBinder<T> ignored = null)
+        {
+            foreach (var bindable in _binders)
+            {
+                if (bindable == ignored)
+                    continue;
+
+                bindable.SetValue(value);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void ChangeValueAndNotify(T value)
         {
             _value = value;
             OnValueChanged?.Invoke(value);
