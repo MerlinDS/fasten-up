85616e79db3b4f7b5205f4883b28a36a8402f089
MerlinDS <merlinkolv@gmail.com>
2023-12-24 14:13:28 +0200

    Added documentation to the BindableEvent.cs

--- Assets/FastenUp/Runtime/Bindables/BindableEvent.cs
+++ Assets/FastenUp/Runtime/Bindables/BindableEvent.cs
@@ -2,10 +2,17 @@
 
 namespace FastenUp.Runtime.Bindables
 {
+    /// <summary>
+    /// Bindable event that provides the ability to subscribe to the events with one argument in the bind Unity components.
+    /// </summary>
+    /// <typeparam name="T">Type of the argument of the <see cref="UnityEvent{T}"/></typeparam>
     public sealed class BindableEvent<T> : BaseBindableEvent<UnityAction<T>>
     {
     }
 
+    /// <summary>
+    /// Bindable event that provides the ability to subscribe to the events without any arguments in the bind Unity components.
+    /// </summary>
     public sealed class BindableEvent : BaseBindableEvent<UnityAction>
     {
     }
