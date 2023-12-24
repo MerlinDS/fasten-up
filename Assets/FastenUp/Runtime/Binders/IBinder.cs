e80b676623c037f1b25e7bfa0ca88eb5ad88fffa
MerlinDS <merlinkolv@gmail.com>
2023-12-24 09:15:34 +0200

    Moved OnBinderChanged event to the binder itself. Update IBinder documentation.

--- Assets/FastenUp/Runtime/Binders/IBinder.cs
+++ Assets/FastenUp/Runtime/Binders/IBinder.cs
@@ -1,15 +1,27 @@
-using FastenUp.Runtime.Bindables;
+using FastenUp.Runtime.Delegates;
 using FastenUp.Runtime.Mediators;
 
 namespace FastenUp.Runtime.Binders
 {
     /// <summary>
-    /// Implementations of this interface will be bound to an <see cref="IBindable{T}"/>.
-    /// It provides communication between the unity components and bindable property in a <see cref="IMediator"/>.
+    /// Binder is a component that can be bind to the bindable property in the <see cref="IMediator"/>.
     /// </summary>
     public interface IBinder
     {
+        /// <summary>
+        /// The name of the bindable property in the <see cref="IMediator"/>.
+        /// </summary>
+        /// <remarks>
+        /// If the name is empty, the binder will ignore the binding and log an error.
+        /// To ignore the binding without logging an error, set the name that starts with the # symbol.
+        /// </remarks>
         string Name { get; }
+        
+        /// <summary>
+        /// The event will be invoked when the binder is changed somehow (For example, when the value is changed).
+        /// Value in the event is the binder that was changed.
+        /// </summary>
+        public event OnBinderChanged OnBinderChanged;
     }
 
     /// <remarks>Provides one-way binding</remarks>
--- Assets/FastenUp/Runtime/Binders/IGettableBinder.cs
+++ Assets/FastenUp/Runtime/Binders/IGettableBinder.cs
@@ -1,13 +1,10 @@
-﻿using FastenUp.Runtime.Delegates;
-
-namespace FastenUp.Runtime.Binders
+﻿namespace FastenUp.Runtime.Binders
 {
     /// <remarks>Provides two-way binding</remarks>
     /// <typeparam name="T">Type of the value that will be set or get by the bind component</typeparam>
     /// <inheritdoc cref="IBinder"/>
     public interface IGettableBinder<T> : IBinder<T>
     {
-        public event OnBinderChanged OnBinderChanged;
         T GetValue();
     }
 }
