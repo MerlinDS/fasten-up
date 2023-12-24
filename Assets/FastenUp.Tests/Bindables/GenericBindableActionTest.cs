b626a8ca1b19179b51cbf278b7f8c0a5b3c3bad0
MerlinDS <merlinkolv@gmail.com>
2023-12-24 13:39:30 +0200

    Added unit tests for the BindableAction

---
+++ Assets/FastenUp.Tests/Bindables/GenericBindableActionTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using FastenUp.Runtime.Bindables;
+using FastenUp.Runtime.Binders.Actions;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine.Events;
+
+namespace FastenUp.Tests.Bindables
+{
+    [TestFixture]
+    [TestOf(typeof(BindableAction<>))]
+    public class GenericBindableActionTest
+    {
+
+        [Test]
+        public void Invoke_When_has_valid_action_Should_invoke_action()
+        {
+            //Arrange
+            const int expected = 1;
+            var mockActionBinder = Substitute.For<IActionBinder<UnityEvent<int>>>();
+            var mockAction = Substitute.For<UnityEvent<int>>();
+            mockActionBinder.OnAction.Returns(mockAction);
+            var sut = new BindableAction<int>();
+            sut.As<IBindableAction<UnityEvent<int>>>().Bind(mockActionBinder);
+            //Act
+            sut.Invoke(expected);
+            //Assert
+            mockAction.Received(1).Invoke(expected);
+        }
+        
+        [Test]
+        public void Invoke_When_has_no_actions_Should_not_throw_exception()
+        {
+            //Arrange
+            var sut = new BindableAction<int>();
+            Action act = ()=> sut.Invoke(1);
+            //Act & Assert
+            act.Should().NotThrow<Exception>();
+        }
+    }
+}
