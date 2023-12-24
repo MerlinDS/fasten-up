ddacc085126daca4172b7f1b7f110c26a21855a0
MerlinDS <merlinkolv@gmail.com>
2023-12-24 14:00:00 +0200

    Renamed ActionBooleanBinder.cs to BooleanActionBinder.cs, added unit tests for it

---
+++ Assets/FastenUp.Tests/Binders/Actions/BooleanActionBinderTest.cs
@@ -0,0 +1,25 @@
+﻿using FastenUp.Runtime.Binders.Actions;
+using FluentAssertions;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace FastenUp.Tests.Binders.Actions
+{
+    [TestFixture]
+    [TestOf(typeof(BooleanActionBinder))]
+    public class BooleanActionBinderTest
+    {
+
+        [Test]
+        public void OnAction_Should_return_UnityEvent()
+        {
+            //Arrange
+            var gameObject = new GameObject(nameof(BooleanActionBinderTest));
+            var sut = gameObject.AddComponent<BooleanActionBinder>();
+            //Act & Assert
+            sut.OnAction.Should().NotBeNull();
+            sut.OnAction.Should().BeOfType<UnityEvent<bool>>();
+        }
+    }
+}
--- Assets/FastenUp/Runtime/Binders/Actions/BooleanActionBinder.cs
+++ Assets/FastenUp/Runtime/Binders/Actions/BooleanActionBinder.cs
@@ -3,7 +3,7 @@ namespace FastenUp.Runtime.Binders.Actions
     /// <summary>
     /// <see cref="ActionBinder{T}"/> for <see cref="bool"/> type.
     /// </summary>
-    public class ActionBooleanBinder : ActionBinder<bool>
+    public class BooleanActionBinder : ActionBinder<bool>
     {
         
     }
