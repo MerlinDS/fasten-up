25c0508e79f183dbdd077863e56e06a34adfb853
MerlinDS <merlinkolv@gmail.com>
2023-12-24 14:00:39 +0200

    Renamed ActionIntBinder to IntActionBinder , added unit tests for it

---
+++ Assets/FastenUp.Tests/Binders/Actions/IntActionBinderTest.cs
@@ -0,0 +1,25 @@
+﻿using FastenUp.Runtime.Binders.Actions;
+using FluentAssertions;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace FastenUp.Tests.Binders.Actions
+{
+    [TestFixture]
+    [TestOf(typeof(IntActionBinder))]
+    public class IntActionBinderTest
+    {
+
+        [Test]
+        public void OnAction_Should_return_UnityEvent()
+        {
+            //Arrange
+            var gameObject = new GameObject(nameof(BooleanActionBinderTest));
+            var sut = gameObject.AddComponent<IntActionBinder>();
+            //Act & Assert
+            sut.OnAction.Should().NotBeNull();
+            sut.OnAction.Should().BeOfType<UnityEvent<int>>();
+        }
+    }
+}
