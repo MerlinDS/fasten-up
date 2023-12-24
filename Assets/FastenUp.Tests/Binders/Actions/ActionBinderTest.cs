0f668666c13fbe789f43b4912058b23171efc769
MerlinDS <merlinkolv@gmail.com>
2023-12-24 13:54:18 +0200

    Added unit tests for the ActionBinder

---
+++ Assets/FastenUp.Tests/Binders/Actions/ActionBinderTest.cs
@@ -0,0 +1,24 @@
+﻿using FastenUp.Runtime.Binders.Actions;
+using FluentAssertions;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace FastenUp.Tests.Binders.Actions
+{
+    [TestFixture]
+    [TestOf(typeof(ActionBinder))]
+    public class ActionBinderTest
+    {
+        [Test]
+        public void OnAction_Should_return_UnityEvent()
+        {
+            //Arrange
+            var gameObject = new GameObject(nameof(ActionBinderTest));
+            var sut = gameObject.AddComponent<ActionBinder>();
+            //Act & Assert
+            sut.OnAction.Should().NotBeNull();
+            sut.OnAction.Should().BeOfType<UnityEvent>();
+        }
+    }
+}
---
+++ Assets/FastenUp.Tests/Binders/Actions/GenericActionBinderTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using FastenUp.Runtime.Binders.Actions;
+using FluentAssertions;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.TestTools;
+
+namespace FastenUp.Tests.Binders.Actions
+{
+    [TestFixture]
+    [TestOf(typeof(ActionBinder<>))]
+    public class GenericActionBinderTest
+    {
+        [Test]
+        public void OnAction_Should_return_UnityEvent()
+        {
+            //Arrange
+            var sut = TestBehaviour.Create();
+            //Act & Assert
+            sut.OnAction.Should().NotBeNull();
+            sut.OnAction.Should().BeOfType<UnityEvent<int>>();
+        }
+        
+        private class TestBehaviour : ActionBinder<int>, IMonoBehaviourTest
+        {
+            /// <inheritdoc />
+            [ExcludeFromCodeCoverage]
+            public bool IsTestFinished => true;
+            
+            public static ActionBinder<int> Create()
+            {
+                var go = new GameObject(nameof(GenericActionBinderTest));
+                return go.AddComponent<TestBehaviour>();
+            }
+        }
+    }
+}
