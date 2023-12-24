38a52f50e496e455474ee75b381db82552049121
MerlinDS <merlinkolv@gmail.com>
2023-12-24 14:10:25 +0200

    Added documentation to the BaseBindableEvent.cs

--- Assets/FastenUp/Runtime/Bindables/BaseBindableEvent.cs
+++ Assets/FastenUp/Runtime/Bindables/BaseBindableEvent.cs
@@ -5,15 +5,28 @@ using FastenUp.Runtime.Exceptions;
 
 namespace FastenUp.Runtime.Bindables
 {
+    /// <summary>
+    /// Base class for bindable events that provides the ability to subscribe to the event in the bind Unity components.
+    /// </summary>
+    /// <typeparam name="T">Type of the delegate to subscribe that is used in the bind Unity components.</typeparam>
     public abstract class BaseBindableEvent<T> : IBindableEvent<T>, IDisposable
     {
         private readonly HashSet<IEventBinder<T>> _listeners = new(1);
 
         private readonly List<T> _actions = new(1);
 
+        /// <summary>
+        /// Checks if the event has any listeners.
+        /// </summary>
+        /// <param name="action"> The action to check for listeners.</param>
+        /// <returns>True if the listener was subscribed to the event. False otherwise.</returns>
         public bool HasListeners(T action) =>
             _actions.Contains(action);
 
+        /// <summary>
+        /// Subscribes the listener to the event.
+        /// </summary>
+        /// <param name="action">The action to subscribe to the event.</param>
         public void AddListener(T action)
         {
             _actions.Add(action);
@@ -21,6 +34,10 @@ namespace FastenUp.Runtime.Bindables
                 listener.AddListener(action);
         }
 
+        /// <summary>
+        /// Unsubscribes the listener from the event.
+        /// </summary>
+        /// <param name="action">The action to unsubscribe from the event.</param>
         public void RemoveListener(T action)
         {
             _actions.Remove(action);
@@ -50,6 +67,7 @@ namespace FastenUp.Runtime.Bindables
             _listeners.Remove(eventBinder);
         }
 
+        /// <inheritdoc />
         public void Dispose()
         {
             foreach (var listener in _listeners)
