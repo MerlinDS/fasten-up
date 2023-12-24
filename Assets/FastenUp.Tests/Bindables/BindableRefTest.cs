41152b89e6f6d1404a5f0369ea4276a9a3c6625f
MerlinDS <merlinkolv@gmail.com>
2023-12-24 11:28:02 +0200

    Implemented BinderRef for the purposes of objects binding

---
+++ Assets/FastenUp.Tests/Bindables/BindableRefTest.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using FastenUp.Runtime.Bindables;
+using FastenUp.Runtime.Binders;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace FastenUp.Tests.Bindables
+{
+    [TestFixture]
+    [TestOf(typeof(BindableRef<>))]
+    public class BindableRefTest
+    {
+        [Test]
+        public void Bind_When_binder_is_null_Should_not_throw_exception()
+        {
+            //Arrange
+            var sut = new BindableRef<TestReference>();
+            Action act = () => sut.As<IBindable<TestReference>>().Bind(null);
+            //Act & Assert
+            act.Should().NotThrow<Exception>();
+        }
+
+        [Test]
+        public void Unbind_When_binder_is_null_Should_not_throw_exception()
+        {
+            //Arrange
+            var sut = new BindableRef<TestReference>();
+            Action act = () => sut.As<IBindable<TestReference>>().Unbind(null);
+            //Act & Assert
+            act.Should().NotThrow<Exception>();
+        }
+
+        [Test]
+        public void Bind_When_binder_is_not_IValueProvider_Should_not_throw_exception()
+        {
+            //Arrange
+            var sut = new BindableRef<TestReference>();
+            Action act = () => sut.As<IBindable<TestReference>>().Bind(Substitute.For<IBinder<TestReference>>());
+            //Act & Assert
+            act.Should().NotThrow<Exception>();
+        }
+
+        [Test]
+        public void Unbind_When_binder_is_not_IValueProvider_Should_not_throw_exception()
+        {
+            //Arrange
+            var sut = new BindableRef<TestReference>();
+            Action act = () => sut.As<IBindable<TestReference>>().Unbind(Substitute.For<IBinder<TestReference>>());
+            //Act & Assert
+            act.Should().NotThrow<Exception>();
+        }
+        
+        [Test]
+        public void Bind_When_binder_is_IValueProvider_Should_set_value_and_invoke_OnRefChanged()
+        {
+            //Arrange
+            var expected = Substitute.For<TestReference>();
+            var binder = Substitute.For<IValueProvider<TestReference>>();
+            binder.GetValue().Returns(expected);
+            
+            var sut = new BindableRef<TestReference>();
+            var onRefChanged = Substitute.For<Action>();
+            sut.OnRefChanged += onRefChanged;
+            //Act
+            sut.As<IBindable<TestReference>>().Bind(binder);
+            //Assert
+            sut.Value.Should().Be(expected);
+            onRefChanged.Received(1).Invoke();
+        }
+        
+        [Test]
+        public void Unbind_When_binder_is_IValueProvider_and_value_equals_provided_value_Should_set_value_to_null_and_invoke_OnRefChanged()
+        {
+            //Arrange
+            var expected = Substitute.For<TestReference>();
+            var binder = Substitute.For<IValueProvider<TestReference>>();
+            binder.GetValue().Returns(expected);
+            
+            var sut = new BindableRef<TestReference>();
+            var onRefChanged = Substitute.For<Action>();
+            sut.OnRefChanged += onRefChanged;
+            
+            sut.As<IBindable<TestReference>>().Bind(binder);
+            onRefChanged.ClearReceivedCalls();
+            //Act
+            sut.As<IBindable<TestReference>>().Unbind(binder);
+            //Assert
+            sut.Value.Should().BeNull();
+            onRefChanged.Received(1).Invoke();
+        }
+        
+        [Test]
+        public void Unbind_When_binder_is_IValueProvider_and_value_not_equals_provided_value_Should_not_set_value_to_null_and_not_invoke_OnRefChanged()
+        {
+            //Arrange
+            var expected = Substitute.For<TestReference>();
+            var binder = Substitute.For<IValueProvider<TestReference>>();
+            binder.GetValue().Returns(expected);
+            
+            var sut = new BindableRef<TestReference>();
+            var onRefChanged = Substitute.For<Action>();
+            sut.OnRefChanged += onRefChanged;
+            
+            sut.As<IBindable<TestReference>>().Bind(binder);
+            onRefChanged.ClearReceivedCalls();
+            binder.GetValue().Returns(Substitute.For<TestReference>());
+            //Act
+            sut.As<IBindable<TestReference>>().Unbind(binder);
+            //Assert
+            sut.Value.Should().NotBeNull();
+        }
+        
+        [Test]
+        public void Unbind_When_binder_is_IValueProvider_and_value_is_null_Should_not_set_value_to_null_and_not_invoke_OnRefChanged()
+        {
+            //Arrange
+            var binder = Substitute.For<IValueProvider<TestReference>>();
+            binder.GetValue().Returns(Substitute.For<TestReference>());
+            
+            var sut = new BindableRef<TestReference>();
+            var onRefChanged = Substitute.For<Action>();
+            sut.OnRefChanged += onRefChanged;
+            //Act
+            sut.As<IBindable<TestReference>>().Unbind(binder);
+            //Assert
+            sut.Value.Should().BeNull();
+        }
+
+
+        internal abstract class TestReference
+        {
+        }
+    }
+}
---
+++ Assets/FastenUp/Runtime/Bindables/BindableRef.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using FastenUp.Runtime.Binders;
+
+namespace FastenUp.Runtime.Bindables
+{
+    /// <summary>
+    /// Bindable reference is used to bind the reference of the specific type to the mediator.
+    /// It can be used, for example, to bind the reference of one mediator to another mediator.
+    /// </summary>
+    /// <typeparam name="T">The type of the referenced object.</typeparam>
+    public sealed class BindableRef<T> : IBindable<T>
+        where T : class
+    {
+        /// <summary>
+        /// This event will be invoked when the referenced object is changed.
+        /// </summary>
+        public event Action OnRefChanged;
+
+        /// <summary>
+        /// The referenced object.
+        /// </summary>
+        public T Value { get; private set; }
+
+        /// <inheritdoc />
+        void IBindable<T>.Bind(IBinder<T> binder)
+        {
+            if (binder is not IValueProvider<T> valueReceiver)
+                return;
+
+            Value = valueReceiver.GetValue();
+            OnRefChanged?.Invoke();
+        }
+
+        /// <inheritdoc />
+        void IBindable<T>.Unbind(IBinder<T> binder)
+        {
+            if (binder is not IValueProvider<T> valueReceiver)
+                return;
+
+            if (Value is null || Value != valueReceiver.GetValue())
+                return;
+
+            Value = null;
+            OnRefChanged?.Invoke();
+        }
+    }
+}
