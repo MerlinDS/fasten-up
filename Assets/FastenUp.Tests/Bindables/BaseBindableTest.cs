f8cace527d763b3f3d9f5894a6821533da403288
MerlinDS <merlinkolv@gmail.com>
2023-12-25 16:04:49 +0200

    Implemented BaseBindable.cs

---
+++ Assets/FastenUp.Tests/Bindables/BaseBindableTest.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using FastenUp.Runtime.Bindables;
+using FastenUp.Runtime.Binders;
+using FastenUp.Runtime.Exceptions;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace FastenUp.Tests.Bindables
+{
+    [TestFixture]
+    [TestOf(typeof(BaseBindable<>))]
+    public class BaseBindableTest
+    {
+
+        [Test]
+        public void Bind_When_binder_was_not_bind_Should_set_value()
+        {
+            //Arrange
+            var binder = Substitute.For<IValueReceiver<bool>>();
+            var sut = Substitute.ForPartsOf<BaseBindable<bool>>(false);
+            //Act & Assert
+            sut.As<IBindable<bool>>().Bind(binder);
+            binder.Received(1).SetValue(false);
+        }
+        
+        [Test]
+        public void Bind_When_binder_already_was_bind_Should_throw_exception()
+        {
+            //Arrange
+            var binder = Substitute.For<IBinder<bool>>();
+            var sut = Substitute.ForPartsOf<BaseBindable<bool>>(false);
+            sut.As<IBindable<bool>>().Bind(binder);
+            Action act = () => sut.As<IBindable<bool>>().Bind(binder);
+            //Act & Assert
+            act.Should().Throw<FastenUpException>();
+        }
+        
+        [Test]
+        public void Unbind_When_binder_was_bind_Should_not_throw_exception()
+        {
+            //Arrange
+            var binder = Substitute.For<IBinder<bool>>();
+            var sut = Substitute.ForPartsOf<BaseBindable<bool>>(false);
+            sut.As<IBindable<bool>>().Bind(binder);
+            Action act = () => sut.As<IBindable<bool>>().Unbind(binder);
+            //Act & Assert
+            act.Should().NotThrow<Exception>();
+        }
+        
+        [Test]
+        public void Unbind_When_binder_was_not_bind_Should_throw_exception()
+        {
+            //Arrange
+            var binder = Substitute.For<IBinder<bool>>();
+            var sut = Substitute.ForPartsOf<BaseBindable<bool>>(false);
+            Action act = () => sut.As<IBindable<bool>>().Unbind(binder);
+            //Act & Assert
+            act.Should().Throw<FastenUpException>();
+        }
+        
+        [Test]
+        public void Value_When_set_Should_notify_all_bindables()
+        {
+            //Arrange
+            var binder1 = Substitute.For<IValueReceiver<bool>>();
+            var binder2 = Substitute.For<IValueReceiver<bool>>();
+            var sut = new Bindable<bool>();
+            sut.As<IBindable<bool>>().Bind(binder1);
+            sut.As<IBindable<bool>>().Bind(binder2);
+            //Act
+            sut.Value = true;
+            //Assert
+            binder1.Received(1).SetValue(true);
+            binder2.Received(1).SetValue(true);
+        }
+    }
+}
--- Assets/FastenUp/Runtime/Bindables/Bindable.cs
+++ Assets/FastenUp/Runtime/Bindables/Bindable.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using FastenUp.Runtime.Binders;
-using FastenUp.Runtime.Exceptions;
 
 namespace FastenUp.Runtime.Bindables
 {
@@ -10,27 +7,20 @@ namespace FastenUp.Runtime.Bindables
     /// Bindable that provides the ability to bind values in the Unity components.
     /// </summary>
     /// <typeparam name="T">Type of the value</typeparam>
-    public sealed class Bindable<T> : IBindable<T>
+    public sealed class Bindable<T> : BaseBindable<T>
     {
-        private readonly HashSet<IBinder<T>> _binders = new(1);
-
-        private T _value;
-
-        public Bindable(T value = default)
+        public Bindable(T value = default) : base(value)
         {
-            _value = value;
         }
 
-        /// <summary>
-        /// The value that will be bind to the Unity components.
-        /// </summary>
-        public T Value
+        /// <inheritdoc />
+        public override T Value
         {
-            get => _value;
+            get => base.Value;
             set
             {
-                UpdateBinders(value);
-                ChangeValueAndNotify(value);
+                base.Value = value;
+                OnValueChanged?.Invoke(value);
             }
         }
 
@@ -39,30 +29,18 @@ namespace FastenUp.Runtime.Bindables
         /// </summary>
         public event Action<T> OnValueChanged;
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        void IBindable<T>.Bind(IBinder<T> binder)
+        /// <inheritdoc />
+        protected override void PostBind(IBinder<T> binder)
         {
-            if (_binders.Contains(binder))
-                throw new FastenUpException($"{nameof(binder)} already bind to the {nameof(Bindable<T>)}.");
-
-            if(binder is IValueReceiver<T> valueReceiver)
-                valueReceiver.SetValue(_value);
-            
-            _binders.Add(binder);
             binder.OnBinderChanged += OnValueChangedHandler;
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        void IBindable<T>.Unbind(IBinder<T> binder)
+        /// <inheritdoc />
+        protected override void PostUnbind(IBinder<T> binder)
         {
-            if (!_binders.Contains(binder))
-                throw new FastenUpException($"{nameof(binder)} not bind to the {nameof(Bindable<T>)}.");
-
-            _binders.Remove(binder);
-            
             binder.OnBinderChanged -= OnValueChangedHandler;
         }
-
+        
         private void OnValueChangedHandler(IBinder binder)
         {
             if (binder is not IValueProvider<T> valueProvider)
@@ -70,26 +48,7 @@ namespace FastenUp.Runtime.Bindables
 
             var value = valueProvider.GetValue();
             UpdateBinders(value, valueProvider);
-            ChangeValueAndNotify(value);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private void UpdateBinders(T value, IBinder<T> ignored = null)
-        {
-            foreach (var bindable in _binders)
-            {
-                if (bindable == ignored || bindable is not IValueReceiver<T> valueReceiver)
-                    continue;
-
-                valueReceiver.SetValue(value);
-            }
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private void ChangeValueAndNotify(T value)
-        {
-            _value = value;
-            OnValueChanged?.Invoke(value);
+            SetValueSilently(value);
         }
     }
 }
