f721a7d0b59f6c2496794f2a3be5b9ae57904e4c
MerlinDS <merlinkolv@gmail.com>
2023-12-25 12:50:44 +0200

    Added IBindableRef.cs and changed implementation of the BindableRef.cs

---
+++ Assets/FastenUp/Runtime/Bindables/IBindableRef.cs
@@ -0,0 +1,14 @@
+﻿namespace FastenUp.Runtime.Bindables
+{
+    /// <summary>
+    /// Implementation of this interface provides the ability to bind object references to a
+    /// <see cref="FastenUp.Runtime.Mediators.IMediator"/>.
+    /// </summary>
+    /// <typeparam name="T">Type of the reference</typeparam>
+    public interface IBindableRef<in T> : IBindable
+    {
+        internal void Bind(T reference);
+
+        internal void Unbind(T reference);
+    }
+}
