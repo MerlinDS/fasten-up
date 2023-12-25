22dd732a568653275cd28b4d57ecd6ac8a7016f9
MerlinDS <merlinkolv@gmail.com>
2023-12-25 16:08:22 +0200

    Implemented BindableSetup

---
+++ Assets/FastenUp.Tests/Bindables/BindableSetupTest.cs
@@ -0,0 +1,19 @@
+﻿using FastenUp.Runtime.Bindables;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace FastenUp.Tests.Bindables
+{
+    [TestFixture]
+    [TestOf(typeof(BindableSetup<>))]
+    public class BindableSetupTest
+    {
+
+        [Test]
+        public void Ctor_When_value_is_not_null_Should_set_value()
+        {
+            //Arrange & Act & Assert
+            new BindableSetup<int>(1).Value.Should().Be(1);
+        }
+    }
+}
