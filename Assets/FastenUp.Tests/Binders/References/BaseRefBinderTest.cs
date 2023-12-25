1cdfe7eee052dfa26f28df0d01b4e9a0d934ffa5
MerlinDS <merlinkolv@gmail.com>
2023-12-25 14:51:23 +0200

    Added possibility to set reference directly in the BaseRefBinder.cs

--- Assets/FastenUp.Tests/Binders/References/BaseRefBinderTest.cs
+++ Assets/FastenUp.Tests/Binders/References/BaseRefBinderTest.cs
@@ -4,6 +4,7 @@ using FluentAssertions;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.UI;
 using UnityTestingAssist.Runtime;
 
 namespace FastenUp.Tests.Binders.References
@@ -12,7 +13,6 @@ namespace FastenUp.Tests.Binders.References
     [TestOf(typeof(BaseRefBinder<>))]
     public class BaseRefBinderTest
     {
-
         [Test]
         public void TryGetReference_When_reference_is_not_set_Should_return_false()
         {
@@ -24,7 +24,7 @@ namespace FastenUp.Tests.Binders.References
             result.Should().BeFalse();
             reference.Should().BeNull();
         }
-        
+
         [Test]
         public void TryGetReference_When_reference_is_set_Should_return_true_and_reference()
         {
@@ -37,7 +37,75 @@ namespace FastenUp.Tests.Binders.References
             result.Should().BeTrue();
             reference.Should().Be(expected);
         }
-        
+
+        [Test]
+        public void Awake_When_Reference_property_is_valid_Should_set_reference()
+        {
+            //Arrange
+            var sut = TestImageRefBinder.Create();
+            var expected = new GameObject(nameof(BaseBinderTests) + "Other").AddComponent<Image>();
+            sut.EditSerializable()
+                .Field("Reference", a => a.objectReferenceValue = expected)
+                .Apply();
+            sut.ExecuteAwake();
+            //Act
+            var result = sut.TryGetReference(out Image reference);
+            //Assert
+            result.Should().BeTrue();
+            reference.Should().Be(expected);
+        }
+
+        [Test]
+        public void Awake_When_Reference_property_is_null_valid_Should_set_reference()
+        {
+            //Arrange
+            var sut = TestImageRefBinder.Create();
+            var expected = sut.gameObject.GetComponent<Image>();
+            sut.ExecuteAwake();
+            //Act
+            var result = sut.TryGetReference(out Image reference);
+            //Assert
+            result.Should().BeTrue();
+            reference.Should().Be(expected);
+        }
+
+        [Test]
+        public void Awake_When_Reference_property_is_not_valid_Should_set_reference()
+        {
+            //Arrange
+            var sut = TestImageRefBinder.Create();
+            var expected = sut.gameObject.GetComponent<Image>();
+            sut.EditSerializable()
+                .Field("Reference", a => a.objectReferenceValue = sut.gameObject)
+                .Apply();
+            sut.ExecuteAwake();
+            //Act
+            var result = sut.TryGetReference(out Image reference);
+            //Assert
+            result.Should().BeTrue();
+            reference.Should().Be(expected);
+            LogAssert.Expect(LogType.Warning,
+                $"Reference TestImageRefBinder (UnityEngine.RectTransform) is not of type {nameof(Image)}. Instead, will try to find it as a component on the same GameObject.");
+        }
+
+        [Test]
+        public void Awake_When_Reference_property_is_not_valid_and_component_is_not_found_Should_not_set_reference()
+        {
+            //Arrange
+            var sut = TestImageRefBinder.Create();
+            Object.DestroyImmediate(sut.gameObject.GetComponent<Image>());
+            sut.EditSerializable()
+                .Field("Reference", a => a.objectReferenceValue =
+                    sut.gameObject)
+                .Apply();
+            sut.ExecuteAwake();
+            //Act
+            var result = sut.TryGetReference(out Image reference);
+            //Assert
+            result.Should().BeFalse();
+            reference.Should().BeNull();
+        }
+
         private class TestRefBinder : BaseRefBinder<Transform>, IMonoBehaviourTest
         {
             /// <inheritdoc />
@@ -49,6 +117,21 @@ namespace FastenUp.Tests.Binders.References
                 var gameObject = new GameObject(nameof(TestRefBinder));
                 var testRefBinder = gameObject.AddComponent<TestRefBinder>();
                 testRefBinder.ExecuteAwake();
+                return testRefBinder;
+            }
+        }
+
+        private class TestImageRefBinder : BaseRefBinder<Image>, IMonoBehaviourTest
+        {
+            /// <inheritdoc />
+            [ExcludeFromCodeCoverage]
+            public bool IsTestFinished => true;
+
+            public static TestImageRefBinder Create()
+            {
+                var gameObject = new GameObject(nameof(TestImageRefBinder));
+                gameObject.AddComponent<Image>();
+                var testRefBinder = gameObject.AddComponent<TestImageRefBinder>();
                 return testRefBinder;
             }
         }
--- Assets/FastenUp/Runtime/Binders/References/BaseRefBinder.cs
+++ Assets/FastenUp/Runtime/Binders/References/BaseRefBinder.cs
@@ -1,24 +1,49 @@
-﻿namespace FastenUp.Runtime.Binders.References
+﻿using UnityEngine;
+
+namespace FastenUp.Runtime.Binders.References
 {
     public abstract class BaseRefBinder<TRef> : BaseBinder, IRefBinder
     {
-        private TRef _reference;//TODO: Make this settable from the inspector.
-        
+        private TRef _reference;
+
+        /// <summary>
+        /// Property that is used to set the reference from the inspector.
+        /// </summary>
+        [field: SerializeReference]
+        private Component Reference { get; set; }
+
         private void Awake()
         {
-            _reference = GetComponent<TRef>();
+            if (Reference is not TRef reference)
+            {
+                ValidateReferenceType();
+                _reference = GetComponent<TRef>();
+                Reference = _reference as Component;
+                return;
+            }
+
+            _reference = reference;
+        }
+
+        private void ValidateReferenceType()
+        {
+            if (Reference == null) 
+                return;
+            
+            Debug.LogWarningFormat(
+                "Reference {0} is not of type {1}. Instead, will try to find it as a component on the same GameObject.",
+                Reference, typeof(TRef).Name);
         }
 
         /// <inheritdoc />
         public bool TryGetReference<T>(out T reference)
         {
             reference = default;
-            if (_reference is not T @ref) 
+            if (_reference is not T @ref)
                 return false;
-            
+
             reference = @ref;
             return true;
-
         }
     }
 }
