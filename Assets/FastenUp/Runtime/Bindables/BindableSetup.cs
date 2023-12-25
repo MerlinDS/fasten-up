ca80c0282cc18e42874ade8767313eeae1581d1b
MerlinDS <merlinkolv@gmail.com>
2023-12-25 16:08:22 +0200

    Implemented BindableSetup

---
+++ Assets/FastenUp/Runtime/Bindables/BindableSetup.cs
@@ -0,0 +1,14 @@
+﻿namespace FastenUp.Runtime.Bindables
+{
+    /// <summary>
+    /// Used to setup the binder during runtime.
+    /// </summary>
+    /// <typeparam name="T">Type of the setup value</typeparam>
+    public sealed class BindableSetup<T> : BaseBindable<T>
+    {
+        /// <inheritdoc />
+        public BindableSetup(T value = default) : base(value)
+        {
+        }
+    }
+}
