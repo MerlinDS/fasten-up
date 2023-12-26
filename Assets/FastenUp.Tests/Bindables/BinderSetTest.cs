5ca73f4afd312efe42a156bb8354251988fc86e0
MerlinDS <merlinkolv@gmail.com>
2023-12-26 13:56:55 +0200

    Implemented BinderSet.cs to reduce code duplication, and generalize bindables

---
+++ Assets/FastenUp.Tests/Bindables/BinderSetTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using FastenUp.Runtime.Bindables;
+using FastenUp.Runtime.Binders;
+using FastenUp.Runtime.Exceptions;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace FastenUp.Tests.Bindables
+{
+    [TestFixture]
+    [TestOf(typeof(BinderSet<>))]
+    public class BinderSetTest
+    {
+        [Test]
+        public void AddBinder_When_binder_not_In_set_Should_adds_binder()
+        {
+            // Arrange
+            var binder = Substitute.For<IBinder>();
+            var sut = new BinderSet<IBinder>();
+            // Act
+            sut.Add(binder);
+            // Assert
+            var enumerator = sut.GetEnumerator();
+            using var disposable = enumerator as IDisposable;
+            enumerator.MoveNext().Should().BeTrue();
+            enumerator.Current.Should().Be(binder);
+        }
+
+        [Test]
+        public void AddBinder_When_binder_In_set_Should_throw_exception()
+        {
+            // Arrange
+            var binder = Substitute.For<IBinder>();
+            var sut = new BinderSet<IBinder>();
+            sut.Add(binder);
+            // Act
+            Action act = () => sut.Add(binder);
+            // Assert
+            act.Should().Throw<FastenUpException>();
+        }
+
+        [Test]
+        public void RemoveBinder_When_binder_In_set_Should_removes_binder()
+        {
+            // Arrange
+            var binder = Substitute.For<IBinder>();
+            var sut = new BinderSet<IBinder>();
+            sut.Add(binder);
+            // Act
+            sut.Remove(binder);
+            // Assert
+            var enumerator = sut.GetEnumerator();
+            using var disposable = enumerator as IDisposable;
+            enumerator.MoveNext().Should().BeFalse();
+        }
+
+        [Test]
+        public void RemoveBinder_When_binder_not_In_set_Should_throw_exception()
+        {
+            // Arrange
+            var binder = Substitute.For<IBinder>();
+            var sut = new BinderSet<IBinder>();
+            // Act
+            Action act = () => sut.Remove(binder);
+            // Assert
+            act.Should().Throw<FastenUpException>();
+        }
+    }
+}
---
+++ Assets/FastenUp/Runtime/Bindables/BinderSet.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using FastenUp.Runtime.Binders;
+using FastenUp.Runtime.Exceptions;
+using JetBrains.Annotations;
+
+namespace FastenUp.Runtime.Bindables
+{
+    /// <summary>
+    /// Internal collection of binders.
+    /// Used to prevent duplicate binders.
+    /// </summary>
+    /// <typeparam name="T">Type of the binder.</typeparam>
+    internal sealed class BinderSet<T>
+        where T : IBinder
+    {
+        private readonly HashSet<T> _binder;
+
+        public BinderSet(int capacity = 1)
+        {
+            _binder = new HashSet<T>(capacity);
+        }
+
+        /// <summary>
+        /// Adds binder to the collection.
+        /// </summary>
+        /// <param name="binder">Binder to add.</param>
+        /// <exception cref="FastenUpException">If binder already bind to the <see cref="IBindable"/>.</exception>
+        public void Add([NotNull] T binder)
+        {
+            if(!_binder.Add(binder))
+                throw new FastenUpException($"{binder} already bind to the {nameof(IBindable)}.");
+        }
+        
+        /// <summary>
+        /// Removes binder from the collection.
+        /// </summary>
+        /// <param name="binder">Binder to remove.</param>
+        /// <exception cref="FastenUpException">If binder not bind to the <see cref="IBindable"/>.</exception>
+        public void Remove([NotNull] T binder)
+        {
+            if(!_binder.Remove(binder))
+                throw new FastenUpException($"{binder} not bind to the {nameof(IBindable)}.");
+        }
+
+        /// <summary>
+        /// Gets the enumerator of the collection.
+        /// </summary>
+        /// <remarks>
+        /// We need to return the enumerator of the <see cref="HashSet{T}"/> to prevent boxing.
+        /// </remarks>
+        public HashSet<T>.Enumerator GetEnumerator()
+        {
+            return _binder.GetEnumerator();
+        }
+    }
+}
